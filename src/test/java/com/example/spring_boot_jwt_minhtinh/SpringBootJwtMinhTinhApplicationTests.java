372081d2e66b131b9cd5c68e0c08217ff267bcfb
Dao Huu Minh Tinh <daotinh0410@gmail.com>
2021-09-24 21:26:29 +0700

    jwt

---
+++ src/test/java/com/example/spring_boot_jwt_minhtinh/SpringBootJwtMinhTinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_minhtinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtMinhTinhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
